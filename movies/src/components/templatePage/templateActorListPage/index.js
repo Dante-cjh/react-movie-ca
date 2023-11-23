09ff4fe806f8e382f98c59330928e26e9102e664
Jiahan Chen <3238468729@qq.com>
2023-11-23 23:05:17 +0000

    add actor search by name filtering

--- movies/src/components/templatePage/templateActorListPage/index.js
+++ movies/src/components/templatePage/templateActorListPage/index.js
@@ -2,15 +2,35 @@ import React, {useState} from "react";
 import Grid from "@mui/material/Grid";
 import Header from "../../movie/headerMovieList";
 import ActorList from "../../actor/actorList";
+import LoginCard from "../../user/LoginCard";
+import FilterCard from "../../actor/filterActorsCard";
 
 function ActorListPageTemplate({actors, action}) {
+    const [nameFilter, setNameFilter] = useState("");
+
+    let displayedActors = actors
+        .filter((a) => {
+            return a.name.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+        });
+
+    const handleChange = (type, value) => {
+        if (type==="name") setNameFilter(value);
+    };
+
     return (
-        <Grid container sx={{ padding: '20px' }}>
+        <Grid container sx={{padding: '20px'}}>
             <Grid item xs={12}>
-                <Header title="Popular People" />
+                <Header title="Popular People"/>
             </Grid>
-            <Grid item container spacing={5}>
-                <ActorList actors={actors} action={action}></ActorList>
+            <Grid item container xs={12} spacing={5} sx={{marginBottom: '20px'}}>
+                <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <LoginCard/>
+                    <FilterCard
+                        onUserInput={handleChange}
+                        nameFilter={nameFilter}
+                    />
+                </Grid>
+                <ActorList actors={displayedActors} action={action}></ActorList>
             </Grid>
         </Grid>
     );
